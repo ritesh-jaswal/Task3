09e715d531c1d63ceacf13c09890025648075802
ritesh <ritesh.jaswal@testingxperts.com>
2024-09-24 13:54:24 +0530

    FIRST COMMIT

---
+++ src/test/java/com/vojtechruzicka/springfoxexample/SpringfoxExampleApplicationTests.java
@@ -0,0 +1,11 @@
+package com.vojtechruzicka.springfoxexample;
+
+
+import org.junit.jupiter.api.Test;
+
+public class SpringfoxExampleApplicationTests {
+	@Test
+	public void contextLoads() {
+	}
+}
+
